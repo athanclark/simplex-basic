e10f45992d2788bfe21353bcfb79cff22f876f85
Athan Clark <athan.clark@gmail.com>
2015-06-10 18:14:47 -0600

    segregated slack variables - populate and makeSlackVars functional

--- src/Linear/Simplex/Basic.hs
+++ src/Linear/Simplex/Basic.hs
@@ -6,6 +6,7 @@ module Linear.Simplex.Basic where
 
 import Linear.Grammar
 import Data.List
+import Data.Bifunctor
 
 import Control.Monad
 import Control.Monad.State
@@ -20,47 +21,77 @@ data Optimize = Max | Min
 -- Objective function should be in the form of @Ax + By + Cz + P = 0@
 simplex :: IneqStdForm -> [IneqStdForm] -> Optimize -> [(LinVar, Double)]
 simplex (LteStd _ _) _ _ = error "Can't run simplex over an inequality - objective function must be literal."
+simplex (GteStd _ _) _ _ = error "Can't run simplex over an inequality - objective function must be literal."
 simplex f cs Max =
   let
-    cs' = evalState (mapM makeSlackVars cs) 0
-    tableau = populate (f:cs')
+    -- objective function is not an inequality, so no slacks will be introduced.
+    tableau = populate $ evalState (mapM makeSlackVars (f:cs)) 0
   in
-  findSubsts $ run tableau
+  getSubst $ run tableau
   where
-    run :: [IneqStdForm] -> [IneqStdForm]
+    run :: [IneqSlack] -> [IneqSlack]
     run = undefined
 
--- | Also translates @Ax >= Q@ to @-Ax <= -Q@.
-makeSlackVars :: MonadState Integer m => IneqStdForm -> m IneqStdForm
-makeSlackVars a@(EquStd _ _) = return a
+    -- finds next index of from objective function
+    nextColumn :: IneqSlack -> Maybe Integer
+    nextColumn = undefined
+
+    -- Extracts resulting data from tableau, excluding junk data
+    getSubst :: [IneqSlack] -> [(LinVar, Double)]
+    getSubst = undefined
+
+-- | Standard-form inequality populated with arbitrary slack variables.
+data IneqSlack = IneqSlack IneqStdForm [LinVar]
+  deriving (Show, Eq)
+
+-- | Also translates @Ax >= Q@ to @-Ax <= -Q@. Ie; result will __exclude__ @GteStd@.
+makeSlackVars :: MonadState Integer m => IneqStdForm -> m IneqSlack
+makeSlackVars a@(EquStd _ _) = return $ IneqSlack a []
 makeSlackVars (LteStd xs xc) = do
   suffix <- get
   put $ suffix + 1
-  return $ EquStd (LinVar ("s" ++ show suffix) 1 : xs) xc
-makeSlackVars (GteStd xs xc) =
-  makeSlackVars $ LteStd (map (\v -> v {varCoeff = varCoeff v * (-1)}) xs) ((-1) * xc)
+  return $ IneqSlack (EquStd xs xc) [LinVar ("s" ++ show suffix) 1]
+makeSlackVars (GteStd xs xc) = -- invert equation, for <= form
+  makeSlackVars $ LteStd (map (\v -> v {varCoeff = varCoeff v * (-1)}) xs)
+                         ((-1) * xc)
 
 -- | Fills missing variables
-populate :: [IneqStdForm] -> [IneqStdForm]
-populate [] = []
-populate xs = let names = nub $ concatMap linVarNames xs in
-  map (fill names) xs
-  where
-    linVarNames :: IneqStdForm -> [String]
-    linVarNames (EquStd xs _) = map varName xs
-    linVarNames (LteStd xs _) = map varName xs
+populate :: [IneqSlack] -> [IneqSlack]
+populate xs =
+  let
+    exauhstive :: ([[String]], [[String]])
+    exauhstive = unzip $ map linVarNames xs
 
-    -- populates missing variables with 0 coefficients, and sorts the result.
-    fill :: [String] -> IneqStdForm -> IneqStdForm
-    fill names x@(EquStd xs xc) = let xnames = linVarNames x in
-      case names \\ xnames of
-        [] -> EquStd (sort xs) xc
-        ns -> EquStd (sort $ xs ++ map (flip LinVar 0) ns) xc
-    fill names x@(LteStd xs xc) = let xnames = linVarNames x in
-      case names \\ xnames of
-        [] -> LteStd (sort xs) xc
-        ns -> LteStd (sort $ xs ++ map (flip LinVar 0) ns) xc
-    fill names x@(GteStd xs xc) = let xnames = linVarNames x in
-      case names \\ xnames of
-        [] -> GteStd (sort xs) xc
-        ns -> GteStd (sort $ xs ++ map (flip LinVar 0) ns) xc
+    allnames :: ([String], [String])
+    allnames = bimap (nub . concat) (nub . concat) exauhstive
+  in
+  map (fill allnames) xs
+  where
+    -- left is user-level vars, right are slack vars
+    linVarNames :: IneqSlack -> ([String], [String])
+    linVarNames (IneqSlack (EquStd xs _) ys) = (map varName xs, map varName ys)
+    linVarNames (IneqSlack (LteStd xs _) ys) = (map varName xs, map varName ys)
+    linVarNames (IneqSlack (GteStd xs _) ys) = (map varName xs, map varName ys)
+
+    -- populates missing variables with @0@ as coefficients, and sorts the result.
+    fill :: ([String], [String]) -> IneqSlack -> IneqSlack
+    fill (rs,ss) x@(IneqSlack (EquStd xs xc) ys) =
+      let (rs',ss') = linVarNames x in
+      case (rs \\ rs', ss \\ ss') of
+        (names,slacks) -> IneqSlack
+          -- instantiate empty user-level vars
+          (EquStd (sort $ xs ++ map (flip LinVar 0) names) xc)
+          -- instantiate empty slack vars
+          (sort $ ys ++ map (flip LinVar 0) slacks)
+
+    fill (rs,ss) x@(IneqSlack (LteStd xs xc) ys) =
+      let (rs',ss') = linVarNames x in
+      case (rs \\ rs', ss \\ ss') of
+        (names,slacks) -> IneqSlack (LteStd (sort $ xs ++ map (flip LinVar 0) names) xc) $
+                                    sort $ ys ++ map (flip LinVar 0) slacks
+
+    fill (rs,ss) x@(IneqSlack (GteStd xs xc) ys) =
+      let (rs',ss') = linVarNames x in
+      case (rs \\ rs', ss \\ ss') of
+        (names,slacks) -> IneqSlack (GteStd (sort $ xs ++ map (flip LinVar 0) names) xc) $
+                                    sort $ ys ++ map (flip LinVar 0) slacks
